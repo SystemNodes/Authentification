ed6273087e7d04f1b9825cb19854716689c276b2
Patrick <patrick@Patricks-MacBook-Pro.local>
2025-09-29 16:48:08 +0100

    Allowed CORS origin

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./config/database');
+const cors = require('cors');
 const userRouter = require('./routers/userRouter');
 const PORT = process.env.PORT
 const app = express();
 
 app.use(express.json());
+app.use(cors('*'));
 app.use(userRouter);
 
 

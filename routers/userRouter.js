c01dc3686a27b5d47e62a1beb02f03efbd48f4f6
Patrick <patrick@Patricks-MacBook-Pro.local>
2025-09-29 15:54:06 +0100

    Validator Update

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,16 +9,20 @@ const {
     getAll
 } = require('../Controllers/userController');
 const { authentication } = require('../middleware/authentication');
+const { 
+    signUpValidator, 
+    loginValidator 
+} = require('../middleware/validator');
 
 const router = require('express').Router();
 
-router.post('/users', signUp);
+router.post('/users', signUpValidator, signUp);
 router.get('/users/verify/:token', verifyUser);
 router.post('/users/resend-verification', resendVerification);
-router.post('/users/login', login);
+router.post('/users/login', loginValidator, login);
 router.post('/users/forgot-password', forgotPassword);
 router.post('/users/reset/password/:token', resetPassword);
 router.put('/users/update-password', authentication, updatePassword);
 router.get('/users', getAll);
 
-module.exports = router
+module.exports = router;
